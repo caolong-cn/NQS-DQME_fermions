90ea94e06da5aca962e385cbbcfeaefaf60d43c8
caolong <caolong@mail.ustc.edu.cn>
2026-01-09 10:45:16 +0800

    __init__ for the last update

--- src/nqsdqme/solver/__init__.py
+++ src/nqsdqme/solver/__init__.py
@@ -1,4 +1,4 @@
 from .solver import (TDVP, TDVP_mc,
                      SS_pre,SS_ll)
 from .solver_test import TDVP_test,TDVP_mc_test
-from .pinn_solver import (PINNSolverSPOP,PINNSolverAdam)
+from .pinn_solver import (PINNSolverSPOP,PINNSolverAdam,PINNSolverSPOPMutlC)
